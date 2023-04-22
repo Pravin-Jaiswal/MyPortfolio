a2cbeebf1a159ac611787d7b0cc6040188c5bc8f
Pravin <pravin126jaiswal@gmail.com>
2023-04-23 02:03:41 +0530

    added contact form

--- script.js
+++ script.js
@@ -17,3 +17,19 @@ function reveal(){
         }
     }
 }
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbxuw2iKQQFAFbdBjUp4NRuSQi0I_SN8V4Y_rJfBCJqoP3Pf8dTiOxza-BINYCQTfMjh/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById('msg')
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = 'Message Sent successfulyy'
+        setTimeout(function(){
+            msg.innerHTML=""},5000)
+            form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
+
