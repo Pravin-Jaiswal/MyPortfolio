82bb3b2bb343fb6800235584428724400b4900ce
Pravin <pravin126jaiswal@gmail.com>
2023-06-19 18:59:06 +0530

    skills updated

--- script.js
+++ script.js
@@ -33,3 +33,38 @@ function reveal(){
       .catch(error => console.error('Error!', error.message))
   })
 
+  $('.slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          dots: false
+        }
+      }
+    ]
+  });
+
